d0601e77b79c4bdb7c773eeed03a603961e86741
majdalkilany <majdkilany92@gmail.com>
2023-09-02 12:07:32 +0300

    Create new tables and add the data to it

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     date_of_birth DATE,
     escape_attempts INTEGER,
@@ -9,3 +9,19 @@ CREATE TABLE animals (
     weight_kg DECIMAL(10, 2)
 );
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER,
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
